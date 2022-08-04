acb50fc0dab26248c898bc48689752f8922ceafb
NESTOR ARISPE <narispe@uc.cl>
2022-08-04 14:30:20 -0400

    style

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ class Brain:
 
     def control(self):
         # while abs(self.error_dist) > self.margen_dist or abs(self.error_ang) > self.margen_ang:
-        while (camera.a):
+        while (camera.running):
             self.controlpos.error_dist__ = self.controlpos.error_dist_
             self.controlpos.error_dist_ = self.controlpos.error_dist
             self.controlpos.error_dist = np.linalg.norm(camera.robot_center - self.controlpos.posRef)
             self.controlpos.error_ang__ = self.controlpos.error_ang_
             self.controlpos.error_ang_ = self.controlpos.error_ang
             self.controlpos.error_ang = camera.robot_angle - self.controlpos.angRef
-            x, y =self.controlpos.get_control()
+            x, y = self.controlpos.get_control()
             self.msg = f"{int(x)},{int(y)};"
         self.msg = "0,0;"
         print("aaaaaaaaaaaa")
@@ -42,23 +42,24 @@ class Brain:
         self.controltr.start()
 
     def send_info(self):
-        ser = serial.Serial("COM10",baudrate = 38400,timeout = 1)
+        ser = serial.Serial("COM10", baudrate=38400, timeout=1)
         time.sleep(1)
 
-        while(camera.a):
+        while camera.running:
             print(f"Enviando {self.msg}")
             msgEncode = str.encode(self.msg)
             ser.write(msgEncode)
             time.sleep(0.5)
         # Cerramos el puerto serial abierto una vez terminado el codigo
         ser.close()
+
     def control_ref(self):
-        while (camera.a):
+        while camera.running:
             self.set_pos_ref(camera.obj)
-        
+
     def set_pos_ref(self, new_pos):
-        #print((np.array(camera.frame.shape)[:2]/2).astype(int))
-        #self.controlpos.posRef = (np.array(camera.frame.shape)[:2]/2).astype(int)
+        # print((np.array(camera.frame.shape)[:2]/2).astype(int))
+        # self.controlpos.posRef = (np.array(camera.frame.shape)[:2]/2).astype(int)
         self.controlpos.posRef = new_pos
 
 
