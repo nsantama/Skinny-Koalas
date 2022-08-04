ddc2c3a3f1208cd402fa29600a53e65515c2fc87
NICOLAS SANTA MARIA <n.santamaria@uc.cl>
2022-08-04 11:48:22 -0400

    Conexion camara-control

--- camera_read.py
+++ camera_read.py
@@ -10,6 +10,11 @@ text_font = cv2.FONT_HERSHEY_SIMPLEX
 text_scale = 0.5
 text_color = (255, 255, 255)
 text_thick = 2
+robot_center = np.array((0,0))
+robot_angle = 0
+color1_hsv = np.array([0, 0, 0])
+color2_hsv = np.array([0, 0, 0])
+color3_hsv = np.array([0, 0, 0])
 
 
 def write_colors():
@@ -31,7 +36,7 @@ def read_colors():
         color3_hsv = np.load(color)
 
 def _mouseEvent(event, x, y, flags, param):
-    global nClick, color1_hsv, color2_hsv, color3_hsv
+    global nClick, color1_hsv, color2_hsv, color3_hsv, frame
 
     if event == cv2.EVENT_LBUTTONDOWN:
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -87,52 +92,54 @@ def draw_centers(img, mass_center1, mass_center2, mass_center3):
     if mass_center3 is not None:
         cv2.circle(img, mass_center3, 10, color_mc3, -1)
 
+def camerarun():
+    global nClick, color1_hsv, color2_hsv, color3_hsv, nCam, LowerColorError, UpperColorError, \
+        gaussian_ksize, text_font, text_scale, text_color, text_thick, frame, robot_center, robot_angle
+    cap = cv2.VideoCapture(nCam)
+    nClick = 1
+    cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
+    cv2.moveWindow('frame', 30, 100)
+    cv2.namedWindow('res', cv2.WINDOW_AUTOSIZE)
+    cv2.moveWindow('res', 700, 100)
+    cv2.setMouseCallback('frame', _mouseEvent)
+    read_colors()
+    while True:
+        ret, frame = cap.read()
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        Color1Mask = get_mask(hsv, color1_hsv, LowerColorError, UpperColorError)
+        Color2Mask = get_mask(hsv, color2_hsv, LowerColorError, UpperColorError)
+        Color3Mask = get_mask(hsv, color3_hsv, LowerColorError, UpperColorError)
+        Color1Res = cv2.bitwise_and(frame, frame, mask=Color1Mask)
+        Color2Res = cv2.bitwise_and(frame, frame, mask=Color2Mask)
+        Color3Res = cv2.bitwise_and(frame, frame, mask=Color3Mask)
+        res = Color1Res + Color2Res + Color3Res
+        center1 = get_mass_center(Color1Mask)  # Front
+        center2 = get_mass_center(Color2Mask)  # Back
+        center3 = get_mass_center(Color3Mask)  # Ball
+        draw_centers(res, center1, center2, center3)
 
-cap = cv2.VideoCapture(nCam)
-nClick = 1
-cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
-cv2.moveWindow('frame', 30, 100)
-cv2.namedWindow('res', cv2.WINDOW_AUTOSIZE)
-cv2.moveWindow('res', 700, 100)
-cv2.setMouseCallback('frame', _mouseEvent)
-read_colors()
-while True:
-    ret, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    Color1Mask = get_mask(hsv, color1_hsv, LowerColorError, UpperColorError)
-    Color2Mask = get_mask(hsv, color2_hsv, LowerColorError, UpperColorError)
-    Color3Mask = get_mask(hsv, color3_hsv, LowerColorError, UpperColorError)
-    Color1Res = cv2.bitwise_and(frame, frame, mask=Color1Mask)
-    Color2Res = cv2.bitwise_and(frame, frame, mask=Color2Mask)
-    Color3Res = cv2.bitwise_and(frame, frame, mask=Color3Mask)
-    res = Color1Res + Color2Res + Color3Res
-    center1 = get_mass_center(Color1Mask)  # Front
-    center2 = get_mass_center(Color2Mask)  # Back
-    center3 = get_mass_center(Color3Mask)  # Ball
-    draw_centers(res, center1, center2, center3)
+        if center1 is not None and center2 is not None:
+            cv2.line(res, center1, center2, (255, 255, 255), 2)
+            robot_center = np.array((0.5 * (center1 + center2)).astype(int))
+            robot_delta = center1 - center2
+            robot_angle = np.arctan2(robot_delta[1], robot_delta[0])
+            cv2.circle(res, robot_center, 10, (255, 255, 255), 1)
+            cv2.putText(res,  f"Robot: [{robot_center[0]}, {robot_center[1]}, {round(np.rad2deg(robot_angle), 1)}]",
+                        (0, 25), text_font, text_scale, text_color, text_thick)
 
-    if center1 is not None and center2 is not None:
-        cv2.line(res, center1, center2, (255, 255, 255), 2)
-        robot_center = (0.5 * (center1 + center2)).astype(int)
-        robot_delta = center1 - center2
-        robot_angle = np.arctan2(robot_delta[1], robot_delta[0])
-        cv2.circle(res, robot_center, 10, (255, 255, 255), 1)
-        cv2.putText(res,  f"Robot: [{robot_center[0]}, {robot_center[1]}, {round(np.rad2deg(robot_angle), 1)}]",
-                    (0, 25), text_font, text_scale, text_color, text_thick)
+            if center3 is not None:
+                cv2.line(res, robot_center, center3, (255, 255, 255), 2)
+                ball_dist = np.linalg.norm(center3 - robot_center)
+                ball_delta = center3 - robot_center
+                ball_angle = np.arctan2(ball_delta[1], ball_delta[0]) - robot_angle
+                cv2.putText(res, f"Ball: [{round(ball_dist)}, {round(np.rad2deg(ball_angle), 1)}]",
+                            (0, 75), text_font, text_scale, text_color, text_thick)
 
-        if center3 is not None:
-            cv2.line(res, robot_center, center3, (255, 255, 255), 2)
-            ball_dist = np.linalg.norm(center3 - robot_center)
-            ball_delta = center3 - robot_center
-            ball_angle = np.arctan2(ball_delta[1], ball_delta[0]) - robot_angle
-            cv2.putText(res, f"Ball: [{round(ball_dist)}, {round(np.rad2deg(ball_angle), 1)}]",
-                        (0, 75), text_font, text_scale, text_color, text_thick)
+        cv2.imshow('frame', frame)
+        cv2.imshow('res', res)
 
-    cv2.imshow('frame', frame)
-    cv2.imshow('res', res)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
 
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
--- control_pos.py
+++ control_pos.py
@@ -1,93 +1,77 @@
 import time
 import numpy as np
-import serial
 import camera_read as camera
 
 
-Kp_dist = 1
-Ki_dist = 0
-Kd_dist = 0
-Kp_ang = 1
-Ki_ang = 0
-Kd_ang = 0
+class ControlPos:
+    def __init__(self):
+        self.posRef = np.array((0,0))
+        self.angRef = 0
+        self.control_dist = 0
+        self.control_and = 0
 
-error_dist = 0
-error_dist_ = 0
-error_dist__ = 0
-error_ang = 0
-error_ang_ = 0
-error_ang__ = 0
+        self.Kp_dist = 1
+        self.Ki_dist = 0
+        self.Kd_dist = 0
+        self.Kp_ang = 1
+        self.Ki_ang = 0
+        self.Kd_ang = 0
 
-margen_dist = 0.001
-margen_ang = 0.1
+        self.error_dist = 0
+        self.error_dist_ = 0
+        self.error_dist__ = 0
+        self.error_ang = 0
+        self.error_ang_ = 0
+        self.error_ang__ = 0
 
-control_dist_max = 100
-control_dist_min = -100
-control_ang_max = 3
-control_ang_min = -3
+        self.margen_dist = 0.001
+        self.margen_ang = 0.1
 
-t = 0
-t_ = 0
+        self.control_dist_max = 100
+        self.control_dist_min = -100
+        self.control_and_max = 3
+        self.control_and_min = -3
 
-ser = serial.Serial("COM6", baudrate=38400, timeout=10)
+        self.t = 0
+        self.t_ = 0
 
-
-def get_coef(Ts, Kp, Ki, Kd):
-    k0 = Kp * (1 + Ts*Ki + Kd/Ts)
-    k1 = -Kp * (1 + 2*Kd/Ts)
-    k2 = Kp*Kd/Ts
-    return (k0, k1, k2)
-
-
-def get_control(posRef, angRef):
-    global error_dist, error_dist_, error_dist__
-    global error_ang, error_ang_, error_ang__
-    global control_dist, control_dist_
-    global control_ang, control_ang_
-    global t, t_
-
-    error_dist__ = error_dist_
-    error_dist_ = error_dist
-    error_dist = np.linalg.norm(camera.robot_center - posRef)
-    error_ang__ = error_ang_
-    error_ang_ = error_ang
-    error_ang = camera.robot_angle - angRef
-    t = time.time()
-    Ts = t - t_
-    t_ = t
-    k0_dist, k1_dist, k2_dist = get_coef(Ts, Kp_dist, Ki_dist, Kd_dist)
-    k0_ang, k1_ang, k2_ang = get_coef(Ts, Kp_ang, Ki_ang, Kd_ang)
-    control_dist_ = control_dist
-    control_ang_ = control_ang
-    control_dist = control_dist_ + k0_dist*error_dist + k1_dist*error_dist_ + k2_dist*error_dist__
-    control_ang = control_ang_ + k0_ang*error_ang + k1_ang*error_ang_ + k2_ang*error_ang__
-    control_dist = max(control_dist_min, control_dist)
-    control_dist = min(control_dist, control_dist_max)
-    control_ang = max(control_ang_min, control_ang)
-    control_ang = min(control_ang, control_ang_max)
+    def get_coef(self, Ts, Kp, Ki, Kd):
+        k0 = Kp * (1 + Ts*Ki + Kd/Ts)
+        k1 = -Kp * (1 + 2*Kd/Ts)
+        k2 = Kp*Kd/Ts
+        return (k0, k1, k2)
 
 
-def send_control():
-    global control_dist, control_ang
+    def get_control(self):
+        self.t = time.time()
+        time.sleep(0.01)
+        Ts = self.t - self.t_
+        self.t_ = self.t
+        k0_dist, k1_dist, k2_dist = self.get_coef(Ts, self.Kp_dist, self.Ki_dist, self.Kd_dist)
+        k0_ang, k1_ang, k2_ang = self.get_coef(Ts, self.Kp_ang, self.Ki_ang, self.Kd_ang)
+        self.control_dist_ = self.control_dist
+        self.control_and_ = self.control_and
+        self.control_dist = self.control_dist_ + k0_dist*self.error_dist + k1_dist*self.error_dist_ + k2_dist*self.error_dist__
+        self.control_and = self.control_and_ + k0_ang*self.error_ang + k1_ang*self.error_ang_ + k2_ang*self.error_ang__
+        self.control_dist = max(self.control_dist_min, self.control_dist)
+        self.control_dist = min(self.control_dist, self.control_dist_max)
+        self.control_and = max(self.control_and_min, self.control_and)
+        self.control_and = min(self.control_and, self.control_and_max)
 
-    vel_R = control_dist + control_ang
-    vel_L = control_dist - control_ang
-    msg = f"{vel_L},{vel_R};"
-    print(msg)
-    msg_encode = str.encode(msg)
-    ser.write(msg_encode)
-    time.sleep(0.5)
+        vel_R = self.control_dist + self.control_and
+        vel_L = self.control_dist - self.control_and
+        return vel_R, vel_L
+
+        """    def send_control():
+        global control_dist, self.control_and
+
+        msg = f"{vel_L},{vel_R};"
+        print(msg)
+        msg_encode = str.encode(msg)
+        ser.write(msg_encode)
+        time.sleep(0.5)"""
 
 
-def make_control(posRef, angRef):
-    global error_dist, error_ang
-    print(1)
-    get_control(posRef, angRef)
-    print(2)
-    send_control()
-    while abs(error_dist) > margen_dist or abs(error_ang) > margen_ang:
-        get_control(posRef, angRef)
-        send_control()
-
-
-make_control([240,320],0)
+    def make_control(self):
+        while abs(self.error_dist) > self.margen_dist or abs(self.error_ang) > self.margen_ang:
+            self.get_control()
