c8612398f23e394e00370fd0d6f5abd8739bef79
NESTOR ARISPE <narispe@uc.cl>
2022-08-05 14:22:58 -0400

    modularizar brain

--- camera_read.py
+++ camera_read.py
@@ -1,10 +1,8 @@
-from control_pos import ControlPos
 import cv2
-import threading
 import time
-import serial
 import numpy as np
 import os
+from brain import Brain
 import parameters as p
 
 running = True
@@ -116,60 +114,6 @@ def draw_centers(img):
         cv2.circle(img, center4, 10, color_mc4, -1)
     if center3 is not None:
         cv2.circle(img, center5, 10, color_mc5, -1)
-
-
-class Brain:
-    def __init__(self):
-        self.controlpos = ControlPos()
-        self.controltr = threading.Thread(target=self.control, daemon=True)
-        self.sendinfo = threading.Thread(target=self.send_info, daemon=True)
-        self.msg = "0,0;"
-        self.emergency = False
-        self.veld = 0
-        self.veli = 0
-
-    def control(self):
-        controling = True
-        while controling:
-            self.controlpos.t = time.time()
-            time.sleep(0.01)
-            Ts = self.controlpos.t - self.controlpos.t_
-            self.controlpos.t_ = self.controlpos.t
-            self.controlpos.error_dist__ = self.controlpos.error_dist_
-            self.controlpos.error_dist_ = self.controlpos.error_dist
-            self.controlpos.error_dist = np.linalg.norm(robot_center - self.controlpos.posRef) * p.PIX_2_M
-            self.controlpos.error_ang__ = self.controlpos.error_ang_
-            self.controlpos.error_ang_ = self.controlpos.error_ang
-            self.controlpos.error_ang = self.controlpos.angAct - self.controlpos.angRef
-            self.veld, self.veli = self.controlpos.get_control(Ts)
-
-    def start(self):
-        print("paso1")
-        self.sendinfo.start()
-        print("paso2")
-        self.controltr.start()
-
-    def send_info(self):
-        ser = serial.Serial(p.SERIAL_PORT, baudrate=38400, timeout=100)
-        time.sleep(1)
-
-        while True:
-            print(f"Enviando {self.msg}")
-            #print(self.controlpos.error_dist)
-            msgEncode = str.encode(self.msg)
-            ser.write(msgEncode)
-            #time.sleep(0.5)
-        ser.close()
-
-    def control_ref(self):
-        while True:
-            self.set_pos_ref(obj)
-
-    def set_pos_ref(self, new_pos):
-        self.controlpos.posRef = new_pos
-
-    def set_ang(self, new_ang):
-        self.controlpos.angAct = new_ang
 
 
 if __name__ == '__main__':
